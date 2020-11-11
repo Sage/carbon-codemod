53f32ad3e7aef07d3f76cc8c26edd4e61f0d42ad
Jamie Thompson <jamie.thompson@sage.com>
2020-11-11 09:59:07 +0000

    fix(message): remove border prop

--- transforms/message-remove-classic-theme/__testfixtures__/Basic.input.js
+++ transforms/message-remove-classic-theme/__testfixtures__/Basic.input.js
@@ -3,9 +3,9 @@ export default () => <Message as="info" roundedCorners />;
 export const withProps = () => <Message as="info" title="Example" roundedCorners={true} />;
 const as = "info";
 const roundedCorners = true;
-export const asVariable = () => <Message as={as} roundedCorners={roundedCorners} />;
-const props = { as: "info", title: "Example", roundedCorners: true }
-export const spread = () => <Message {...props} roundedCorners />;
-export const ObjectExpressionsLiteralReplacement = () => <Message {...{ as: "info", title: "Example", roundedCorners: true }} />;
-export const ObjectExpressionsIdentifierReplacement1 = () => <Message {...{ as, title: "Example", roundedCorners }} />;
-export const ObjectExpressionsIdentifierReplacement2 = () => <Message {...{ as: as, title: "Example", roundedCorners: roundedCorners }} />;
+export const asVariable = () => <Message as={as} roundedCorners={roundedCorners} border={false}/>;
+const props = { as: "info", title: "Example", roundedCorners: true, border: false }
+export const spread = () => <Message {...props} roundedCorners border={false}/>;
+export const ObjectExpressionsLiteralReplacement = () => <Message {...{ as: "info", title: "Example", roundedCorners: true, border: false }} />;
+export const ObjectExpressionsIdentifierReplacement1 = () => <Message {...{ as, title: "Example", roundedCorners, border: false }} />;
+export const ObjectExpressionsIdentifierReplacement2 = () => <Message {...{ as: as, title: "Example", roundedCorners: roundedCorners, border: false }} />;
