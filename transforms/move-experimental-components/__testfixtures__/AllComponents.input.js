7d796d90e98ada3c2860550f53defb3d8e84959b
mkrds <mkordas@pgs-soft.com>
2021-05-05 20:51:49 +0200

    feat: add move-experimental-components codemod

---
+++ transforms/move-experimental-components/__testfixtures__/AllComponents.input.js
@@ -0,0 +1,22 @@
+import { Checkbox, CheckboxGroup } from "carbon-react/lib/__experimental__/components/checkbox";
+import DateInput from "carbon-react/lib/__experimental__/components/date";
+import DateRange from "carbon-react/lib/__experimental__/components/date-range";
+import Decimal from "carbon-react/lib/__experimental__/components/decimal";
+import Fieldset from "carbon-react/lib/__experimental__/components/fieldset";
+import GroupedCharacter from "carbon-react/lib/__experimental__/components/grouped-character";
+import Number from "carbon-react/lib/__experimental__/components/number";
+import NumeralDate from "carbon-react/lib/__experimental__/components/numeral-date";
+import { RadioButton, RadioButtonGroup } from "carbon-react/lib/__experimental__/components/radio-button";
+import Search from "carbon-react/lib/__experimental__/components/search";
+import { SimpleColorPicker, SimpleColor } from "carbon-react/lib/__experimental__/components/simple-color-picker";
+import Switch from "carbon-react/lib/__experimental__/components/switch";
+import Textarea from "carbon-react/lib/__experimental__/components/textarea";
+import Textbox from "carbon-react/lib/__experimental__/components/textbox";
+
+import FieldHelp from "carbon-react/lib/__experimental__/components/field-help";
+import FormField from "carbon-react/lib/__experimental__/components/form-field";
+import Input from "carbon-react/lib/__experimental__/components/input";
+import InputIconToggle from "carbon-react/lib/__experimental__/components/input-icon-toggle";
+import Label from "carbon-react/lib/__experimental__/components/label";
+
+import SomeOtherRandomComponent from 'carbon-react/lib/components/some-other-random-component'
